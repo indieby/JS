45dbe2bcb7d7fd8c573053c6585ec15a819e9e0f
indieby <massimo.blr@tut.by>
2013-09-26 14:49:29 +0300

    *

--- tags.js
+++ tags.js
@@ -1,2 +1,22 @@
-	$('.page-item-445 > a').removeAttr('href');
-	$('#container').before('<div class="myTags"><a href="http://indie-game.blog.tut.by/tag/kazualki">казуалки</a><a href="http://indie-game.blog.tut.by/tag/cipher-prime">Cipher Prime</a><a href="http://indie-game.blog.tut.by/tag/dear-esther">Dear Esther</a><a href="http://indie-game.blog.tut.by/tag/hotline-miami">Hotline Miami</a><a href="http://indie-game.blog.tut.by/tag/indie-bundle">indie-bundle</a><a href="http://indie-game.blog.tut.by/tag/fez">FEZ</a><a href="http://indie-game.blog.tut.by/tag/filmyi">инди-фильмы</a><a href="http://indie-game.blog.tut.by/tag/indi-foto">инди-фото</a><a href="http://indie-game.blog.tut.by/tag/8-bit">8-bit</a><a href="http://indie-game.blog.tut.by/tag/onlayn-indi">онлайн-инди</a><a href="http://indie-game.blog.tut.by/tag/flesh-indi">флэш-инди</a><a href="http://indie-game.blog.tut.by/tag/horror-indi">хоррор-инди</a><a href="http://indie-game.blog.tut.by/tag/shelter">Shelter</a><a href="http://indie-game.blog.tut.by/tag/video">видео</a><a href="http://indie-game.blog.tut.by/tag/intervyu">интервью</a><a href="http://indie-game.blog.tut.by/tag/android">android</a><a href="http://indie-game.blog.tut.by/tag/limbo">Limbo</a><a href="http://indie-game.blog.tut.by/tag/indi-festivali">инди-фестивали</a></div>');
+$('.page-item-445 > a').removeAttr('href');
+$('#container').before('\
+<div class="myTags">\
+<a href="http://indie-game.blog.tut.by/tag/kazualki">казуалки</a>\
+<a href="http://indie-game.blog.tut.by/tag/cipher-prime">Cipher Prime</a>\
+<a href="http://indie-game.blog.tut.by/tag/dear-esther">Dear Esther</a>\
+<a href="http://indie-game.blog.tut.by/tag/hotline-miami">Hotline Miami</a>\
+<a href="http://indie-game.blog.tut.by/tag/indie-bundle">indie-bundle</a>\
+<a href="http://indie-game.blog.tut.by/tag/fez">FEZ</a>\
+<a href="http://indie-game.blog.tut.by/tag/filmyi">инди-фильмы</a>\
+<a href="http://indie-game.blog.tut.by/tag/indi-foto">инди-фото</a>\
+<a href="http://indie-game.blog.tut.by/tag/8-bit">8-bit</a>\
+<a href="http://indie-game.blog.tut.by/tag/onlayn-indi">онлайн-инди</a>\
+<a href="http://indie-game.blog.tut.by/tag/flesh-indi">флэш-инди</a>\
+<a href="http://indie-game.blog.tut.by/tag/horror-indi">хоррор-инди</a>\
+<a href="http://indie-game.blog.tut.by/tag/shelter">Shelter</a>\
+<a href="http://indie-game.blog.tut.by/tag/video">видео</a>\
+<a href="http://indie-game.blog.tut.by/tag/intervyu">интервью</a>\
+<a href="http://indie-game.blog.tut.by/tag/android">android</a>\
+<a href="http://indie-game.blog.tut.by/tag/limbo">Limbo</a>\
+<a href="http://indie-game.blog.tut.by/tag/indi-festivali">инди-фестивали</a>\
+</div>');
